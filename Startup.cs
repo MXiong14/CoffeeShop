ed0fa0b9f58c61d69e4fc283f04e8a77b9d11db8
Matt <Mattxiong_14@hotmail.com>
2021-10-04 14:14:53 -0500

    added app.useCors in the configure method inside of the startup file

--- Startup.cs
+++ Startup.cs
@@ -45,6 +45,14 @@ namespace CoffeeShop
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoffeeShop v1"));
+
+                // Do not block requests while in development
+                app.UseCors(options =>
+                {
+                    options.AllowAnyOrigin();
+                    options.AllowAnyMethod();
+                    options.AllowAnyHeader();
+                });
             }
 
             app.UseHttpsRedirection();
@@ -58,5 +66,6 @@ namespace CoffeeShop
                 endpoints.MapControllers();
             });
         }
+
     }
 }
